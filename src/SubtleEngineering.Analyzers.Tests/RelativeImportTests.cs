7ef9c36789e5c81890d8d43180209ef7f43a3081
Shahar Prish <shaharp@microsoft.com>
2024-05-04 16:57:09 +0300

    Fix tests.

--- src/SubtleEngineering.Analyzers.Tests/RelativeImportTests.cs
+++ src/SubtleEngineering.Analyzers.Tests/RelativeImportTests.cs
@@ -17,10 +17,6 @@ public class RelativeImportTests
 
             namespace A
             {
-            }
-
-            namespace A
-            {
                 using B.C;
             }
             """;
